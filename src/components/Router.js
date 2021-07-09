bf46a5f551f72910ddefe2ea23b7dae0a188e107
with-key <rem9238@kakao.com>
2021-07-09 18:36:25 +0900

    디테일 페이지 라우터 url추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // components
-import Home from '../page/Home';
+import HomeContainer from '../page/HomeContainer';
+import Article from '../page/Article';
 
 export default function Router() {
 	return (
 		<Switch>
-			<Route path='/' component={Home} exact />
-			{/* 👇 컴포넌트 추가 */}
-			{/* <Route path='/' component={Home} exact /> */}
+			<Route path='/' component={HomeContainer} exact />
+			<Route path='/article/:id' component={Article} exact />
 			{/* <Route path='/' component={Home} exact /> */}
 			{/* <Route path='/' component={Home} exact /> */}
 		</Switch>
