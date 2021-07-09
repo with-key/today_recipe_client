983a933e09cd08408d8055848ce104ce24cf0c46
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-09 19:25:23 +0900

    login ,signup 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,15 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 
 // components
 import Home from '../page/Home';
+import Login from '../page/Login';
+import SignUp from '../page/SignUp';
 
 export default function Router() {
 	return (
 		<Switch>
 			<Route path='/' component={Home} exact />
 			{/* 👇 컴포넌트 추가 */}
-			{/* <Route path='/' component={Home} exact /> */}
-			{/* <Route path='/' component={Home} exact /> */}
-			{/* <Route path='/' component={Home} exact /> */}
+			<Route path='/login' component={Login} exact />
+			<Route path='/signup' component={SignUp} exact />
 		</Switch>
 	);
 }
