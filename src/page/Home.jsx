e0c00701834a3dcb4be532cda006197b9e050d1b
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-09 19:25:08 +0900

    axios 통신 연결중

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Home = () => {
-	return <div>홈</div>;
+	const user = useSelector(state => state.user.user);
+	const is_login = useSelector(state => state.user.is_login);
+	console.log(user)
+
+	axios.get('http://localhost:4000/posts').then(res=>console.log(res));
+
+	axios
+	.post('http://localhost:4000/posts', {"id":7272,"title":"추가연습","body":"추가연습2"})
+	.then(res=>console.log(res))
+	.catch((res)=>console.log('err'));
+
+	if(!is_login){
+		return(<div>로그인 부터 하시죠!!</div>)
+	}
+
+	return <div>로그인 한 사람만 볼 수있징!</div>;
 };
 
 export default Home;
