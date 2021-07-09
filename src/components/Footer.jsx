64b514d45f3e42e180d170fe55d7a51f2fa7ace4
with-key <rem9238@kakao.com>
2021-07-09 18:39:17 +0900

    푸터 컴포넌트 추가

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+	return <Container></Container>;
+};
+
+const Container = styled.footer``;
+export default Footer;
