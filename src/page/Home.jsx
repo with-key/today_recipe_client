4c801d4cded25eae5b9552cb6afc6cddec45ddc7
with-key <rem9238@kakao.com>
2021-07-09 18:45:09 +0900

    홈 컴포넌트 수정 (비동기 테스트 적용)

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-const Home = () => {
-	return <div>Home</div>;
+// compo
+import Template from '../components/Template';
+import Search from '../components/Search';
+
+const Home = ({ articles }) => {
+	if (!articles) {
+		return <div>로딩중....</div>;
+	}
+
+	return (
+		<Template>
+			<Search></Search>
+			{articles.map((item) => (
+				<div>{item.id}</div>
+			))}
+		</Template>
+	);
 };
 
 export default Home;
