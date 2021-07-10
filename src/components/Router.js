f939b792db88447b278dd0ac43898e311484a71f
with-key <rem9238@kakao.com>
2021-07-10 10:51:39 +0900

    라우터 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // components
-import HomeContainer from '../page/HomeContainer';
+import Home from '../page/Home';
 import Article from '../page/Article';
 
 export default function Router() {
 	return (
 		<Switch>
-			<Route path='/' component={HomeContainer} exact />
+			<Route path='/' component={Home} exact />
 			<Route path='/article/:id' component={Article} exact />
 			{/* <Route path='/' component={Home} exact /> */}
 			{/* <Route path='/' component={Home} exact /> */}
