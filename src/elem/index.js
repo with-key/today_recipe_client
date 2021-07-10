21ae7c7188e5d36e6f65a81950f1d6f4e7a80251
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-10 15:57:03 +0900

    엘리먼트 불러오기

--- src/elem/index.js
+++ src/elem/index.js
@@ -1,5 +1,7 @@
 import Button from './Button';
 import Flex from './Flex';
 import Row from './Row';
+import Text from './Text';
+import Input from './Input';
 
-export { Button, Flex, Row };
+export { Button, Flex, Row, Text, Input};
