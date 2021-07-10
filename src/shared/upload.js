13fe85d32ecde152be2e6672962a60758d71de1f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-10 16:35:36 +0900

    AWS s3 작업중

--- src/shared/upload.js
+++ src/shared/upload.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 import styled from 'styled-components';
 import { Text } from '../elem';
+import AWS from 'aws-sdk';
 
 const Upload = (props) => {
+    AWS.config.update({
+        region: "ap-northeast-2",
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: "ap-northeast-2:1341881f-0e47-4578-a076-7cf301309b84",
+        }),
+      })
+
     const fileInput = React.useRef();
     console.log(fileInput);
 
     const selectFile = (e) => {
-        const reader = new FileReader();
         const file = fileInput.current.files[0];
 
-        reader.readAsDataURL(file);
-        console.log(file);
+        console.log(file)
     }
 
 
@@ -30,6 +36,11 @@ const Box = styled.div`
     width: 100%;
     height: 10vh;
 `;
-
+// Amazon Cognito 인증 공급자를 초기화합니다
+// CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+//     getApplicationContext(),
+//     "ap-northeast-2:1341881f-0e47-4578-a076-7cf301309b84", // 자격 증명 풀 ID
+//     Regions.AP_NORTHEAST_2 // 리전
+// );
 
 export default Upload;
