bdf94dd910ffe96d5826b005c6ca46b45103fe00
with-key <rem9238@kakao.com>
2021-07-12 16:40:06 +0900

    컴포넌트 이동 시, 가장 위로 이동 유틸 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Router from './components/Router';
 import GlobalStyles from './components/GlobalStyles';
+import ScrollToTop from './util/ScrollToTop';
 
 function App() {
 	return (
 		<>
+			<ScrollToTop></ScrollToTop>
 			<GlobalStyles />
 			<Router />
 		</>
