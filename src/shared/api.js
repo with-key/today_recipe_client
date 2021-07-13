0263c30e60321201fce2ac5fe59d0e2be1031bd7
with-key <rem9238@kakao.com>
2021-07-13 23:15:56 +0900

    임시 저장

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,35 +1,23 @@
 import axios from 'axios';
 
-// const accessToken = document.cookie.split('=')[1]
+const accessToken = document.cookie.split('=')[1];
 
 const api = axios.create({
-	// 인스턴스
-	// baseURL: 'http://3.34.140.51',
-	baseURL: 'http://localhost:4000/',
+	baseURL: 'http://3.34.140.51',
 	headers: {
 		'content-type': 'application/json;charset=UTF-8',
 		accept: 'application/json,',
+		'X-AUTH-TOKEN': `${accessToken}`,
 	},
 });
 
-// JSESSION 방식
-// const loginApi = axios.create({
-// 	baseURL: 'http://3.34.140.51',
-// 	headers: {
-// 		'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-// 		accept: 'application/json,',
-// 	},
-// })
-
-// api.defaults.headers.common['Authorization'] = `${accessToken}`;
-
 export const apis = {
 	// article
-	add: (contents) => api.post('/articles', contents),
-	edit: (id, contents) => api.put(`/articles/${id}`, contents),
-	del: (id) => api.delete(`/articles/${id}`),
-	articles: () => api.get('/articles'),
-	article: (id) => api.get(`/articles/${id}`),
+	add: (contents) => api.post('/api/articles', contents),
+	edit: (id, contents) => api.put(`api/articles/${id}`, contents),
+	del: (id) => api.delete(`api/articles/${id}`),
+	articles: () => api.get('/api/articles'),
+	article: (id) => api.get(`/api/articles/${id}`),
 
 	// comment
 	addComment: (comment) => api.post('/comments', comment),
@@ -40,25 +28,7 @@ export const apis = {
 			},
 		}),
 
-	// 👇 본 서버
-	// export const apis = {
-	// 	// article
-	// 	add: (contents) => api.post('/api/articles', contents),
-	// 	edit: () => {},
-	// 	articles: () => api.get('/api/articles'),
-	// 	article: (id) => api.get(`/api/articles/${id}`),
-
-	// 	// comment
-	// 	addComment: (comment) => api.post('/comments', comment),
-	// 	comments: (id) =>
-	// 		api.get('/comments/', {
-	// 			params: {
-	// 				postId: id,
-	// 			},
-	// 		}),
-
-	//user
-	// login: (id, pw) => loginApi.post('/user/login',`username=${id}&password=${pw}`,{ withCredentials:true }),
+	// user
 	login: (id, pw) => api.post('/user/login', { username: id, password: pw }),
 	signup: (id, email, pw, pwcheck) =>
 		api.post('/user/signup', {
