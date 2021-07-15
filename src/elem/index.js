827e7eca7ebe2fd0c82e1237af4bdf0ccb509e0f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-15 12:00:36 +0900

    Image.js 추가

--- src/elem/index.js
+++ src/elem/index.js
@@ -4,5 +4,6 @@ import Row from './Row';
 import Text from './Text';
 import Input from './Input';
 import Logo from './Logo';
+import Image from './Image';
 
-export { Button, Flex, Row, Text, Input, Logo };
+export { Button, Flex, Row, Text, Input, Logo, Image };
