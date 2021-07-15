9d4cc3228f0325cf5b9e9965822b515036ea144c
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-15 12:01:06 +0900

    수정하기 뷰 수정 및 이미지 프리뷰 및 이미지 수정 추가

--- src/page/ArticleEdit.jsx
+++ src/page/ArticleEdit.jsx
@@ -1,23 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import { apis } from '../shared/api';
 import styled from 'styled-components';
-import { Text, Input, Button } from '../elem';
+import { Text, Input, Button, Image } from '../elem';
 import Template from '../components/Template';
 import { __editArticle } from '../modules/article';
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import Auth from '../util/Auth';
+import { imageCreators } from '../modules/image';
+
+import AWS from 'aws-sdk';
 
 const ArticleEdit = ({ match, history }) => {
+	const preview = useSelector((state) => state.image.preview);
 	const [contents, setContents] = useState({
 		title: '',
 		content: '',
 		imageUrl: '',
 	});
+	
 	const dispatch = useDispatch();
 
 	const {
 		params: { id },
 	} = match;
+
+	AWS.config.update({
+		region: 'ap-northeast-2',
+		credentials: new AWS.CognitoIdentityCredentials({
+			IdentityPoolId: 'ap-northeast-2:1341881f-0e47-4578-a076-7cf301309b84',
+		}),
+	});
+
+	const fileInput = React.useRef();
+
+	const filePreview = () => {
+		const reader = new FileReader();
+		const file = fileInput.current.files[0];
+
+		reader.readAsDataURL(file);
+		reader.onloadend = () => {
+			dispatch(imageCreators.setPreview(reader.result));
+		};
+	};
+
+	const selectFile = () => {
+		const date = new Date();
+		const file = fileInput.current.files[0];
+
+		if (!file) {
+			window.alert('이미지를 업로드해주세요');
+			return;
+		}
+
+		const upload = new AWS.S3.ManagedUpload({
+			params: {
+				Bucket: 'todayrecipe',
+				Key: file.name + date.getTime() + '.jpg',
+				Body: file,
+			},
+		});
+
+		const promise = upload.promise();
+
+		promise
+			.then((data) => {
+				dispatch(imageCreators.imageUpload(data.Location));
+				const content = {
+					...contents,
+					imageUrl: data.Location,
+				};
+				dispatch(__editArticle(id,content));
+			})
+			.catch((err) => {
+				window.alert('이미지 업로드에 문제가 있어요!', err);
+			});
+	};
 
 	useEffect(() => {
 		const fetchArticle = async () => {
@@ -39,8 +96,28 @@ const ArticleEdit = ({ match, history }) => {
 			<Template>
 				<Container>
 					<Box>
+						<Text fs='20px' mg='0 0 10px 0' fw='600'>
+							이미지 업로드
+						</Text>
+						<label style={{}} htmlFor='fileUpload'>
+							<Image
+								shape='rectangle'
+								src={
+									preview
+										? preview
+										: contents.imageUrl
+								}
+							/>
+						</label>
+						<input
+							style={{ display: 'none' }}
+							type='file'
+							ref={fileInput}
+							onChange={filePreview}
+							id='fileUpload'
+						/>
 						<Grid>
-							<Text size='28px' bold>
+							<Text fs='20px' mg='10px 0 10px 0' fw='600'>
 								제목
 							</Text>
 							<Input
@@ -52,7 +129,7 @@ const ArticleEdit = ({ match, history }) => {
 							/>
 						</Grid>
 						<Grid>
-							<Text size='28px' margin='30px 0 0 0' bold>
+							<Text fs='28px' mg='30px 0 10px 0' fw='600'>
 								레시피
 							</Text>
 							<Input
@@ -80,7 +157,7 @@ const ArticleEdit = ({ match, history }) => {
 								_onClick={() => {
 									const result = window.confirm('정말 이 레시피를 수정할까요?');
 									if (result) {
-										dispatch(__editArticle(id, contents));
+										selectFile()
 									}
 								}}
 							>
